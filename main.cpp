6a0ea25bc9b1c4aa95b8a0e099c5c6fe7f8e9a59
sndth <75499293+sndth@users.noreply.github.com>
2024-08-17 02:07:38 +0200

    Currently vivid does not support exceptions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <sndth/vivid.hpp>
 
-using namespace sndth;
-
 int
 main()
 {
-  try {
-    std::cout << vivid::format("Hello from red{vigreen{vid}} or green{vired{vid}}!");
-  } catch (const std::exception& error) {
-    std::cerr << error.what();
-  }
+  std::cout << sndth::vivid::format(
+    "Hello from red{vigreen{vid}} or green{vired{vid}}!");
 }
